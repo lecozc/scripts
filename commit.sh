9c5741c601603b8a2025f928daa33cce5a0c573b
T0FFF <toff@myhost.fr>
2016-04-18 21:56:52 +0200

    Add script

--- commit.sh
+++ commit.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -x
+#!/bin/sh
 
 usage(){
 	echo "Usage: `basename $0` <message> <name> <email>"	
@@ -9,7 +9,8 @@ usage(){
 
 git add *
 git commit -m "$1"
-git config user.name $2
+git config --global credential.helper 'cache --timeout=3600'
+git config --global user.name $2
 git config --global user.email $3
 
 git push origin master
