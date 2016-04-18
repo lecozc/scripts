6cccefa12989e6eefe99701a528904ab263b5537
root <toff@myhost.fr>
2016-04-18 21:49:46 +0200

    Add script

--- commit.sh
+++ commit.sh
@@ -1,8 +1,16 @@
 #!/bin/sh -x
 
+usage(){
+	echo "Usage: `basename $0` <message> <name> <email>"	
+	exit 1
+}
+
+[ "$#" -eq 3 ] && echo "Starting `basename $0`"|| usage
+
 git add *
 git commit -m "$1"
-git config --global user.email $2
+git config user.name $2
+git config --global user.email $3
 
 git push origin master
 
